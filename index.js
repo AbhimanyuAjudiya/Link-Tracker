7392b5514e71a5aba9b0e445e58894d626ae6781
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-07-12 16:09:13 +0530

    inserted 'save tab' button and used chrome api to get current tab

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
@@ -23,6 +24,15 @@ function render(leads) {
     }
     ulEl.innerHTML = listItems
 }
+
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)    
+    })
+
+})
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
